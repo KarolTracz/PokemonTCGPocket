7885c9c310d4c8f73a761f1cb462a0e425797147
KarolTracz <KTracz00@gmail.com>
2025-08-13 20:50:43 +0200

    PokeDB update

--- PokeDB.py
+++ PokeDB.py
@@ -8,7 +8,7 @@ with open('pokemon_list.json', 'r', encoding='utf-8') as f:
 def main() -> None:
     pokemon_list = change_rarity(raw_pokemon_list)
     create_database(pokemon_list=pokemon_list)
-
+    pass
 
 def change_rarity(sorce_json: list) -> list:
     rarities = {
@@ -20,7 +20,8 @@ def change_rarity(sorce_json: list) -> list:
         '☆☆': '2_star',
         '☆☆☆': '3_star',
         '♕': 'crown',
-        'Promo': 'promo'}
+        'Promo': 'promo'
+    }
 
     last_was_3star = False
     for pokemon in sorce_json:
@@ -32,29 +33,36 @@ def change_rarity(sorce_json: list) -> list:
             pokemon['rarity'] = '1_shiny'
         elif pokemon['rarity'] == '☆☆' and last_was_3star == True:
             pokemon['rarity'] = '2_shiny'
+
         for i in rarities.keys():
             if i == pokemon['rarity']:
                 pokemon['rarity'] = rarities[i]
+
     return sorce_json
 
 
 def create_database(pokemon_list: list) -> None:
     con = sqlite3.connect("PokeDB.db")
     cur = con.cursor()
-    cur.execute("DROP TABLE POKEMONS")
-    cur.execute("CREATE TABLE POKEMONS (card_id, name, pack, rarity, amount, stage)")
+    cur.execute("DROP TABLE pokemons")
+    cur.execute("CREATE TABLE pokemons (id, name, set_num, num, pack, rarity, amount, stage)")
+
     data = []
-    for pokemon in pokemon_list:
+    for i, pokemon in enumerate(pokemon_list):
+
+        _set, num = pokemon['id'].split('-')
         pokemon_data = (
-            pokemon['id'],
+            i,
             pokemon['name'],
+            _set,
+            num,
             pokemon['pack'],
             pokemon['rarity'],
             None,
             None
         )
         data.append(pokemon_data)
-    cur.executemany("INSERT INTO POKEMONS VALUES (?, ?, ?, ?, ?, ?)", data)
+    cur.executemany("INSERT INTO POKEMONS VALUES (?, ?, ?, ?, ?, ?, ?, ?)", data)
     con.commit()
 
 
