61e3f7c81b4d1f2a0311e1665751dc71c3f47874
KarolTracz <KTracz00@gmail.com>
2025-08-14 23:59:20 +0200

    promo separate

--- PokeDB.py
+++ PokeDB.py
@@ -44,12 +44,24 @@ def change_rarity(sorce_json: list) -> list:
 def create_database(pokemon_list: list) -> None:
     con = sqlite3.connect("PokeDB.db")
     cur = con.cursor()
-    cur.execute("DROP TABLE pokemons")
-    cur.execute("CREATE TABLE pokemons (id, name, set_num, num, pack, rarity, amount, stage)")
+    cur.execute("DROP TABLE promo_cards")
+    cur.execute("DROP TABLE normal_carads")
+    cur.execute("CREATE TABLE normal_carads (id, name, set_num, num, pack, rarity, amount, stage)")
+    cur.execute("CREATE TABLE promo_cards (id, name, set_num, num, pack, rarity, amount, stage)")
 
-    data = []
-    for i, pokemon in enumerate(pokemon_list):
 
+    normal_carads = json2db_data_parser([i for i in pokemon_list if i['id'][:2] != 'pa'])
+    promo_cards = json2db_data_parser([i for i in pokemon_list if i['id'][:2] == 'pa'])
+    print(normal_carads[:100])
+    print(promo_cards[:100])
+
+    cur.executemany("INSERT INTO normal_carads VALUES (?, ?, ?, ?, ?, ?, ?, ?)", normal_carads)
+    cur.executemany("INSERT INTO promo_cards VALUES (?, ?, ?, ?, ?, ?, ?, ?)", promo_cards)
+    con.commit()
+
+def json2db_data_parser(json: list) -> list:
+    procesed_cards = []
+    for i, pokemon in enumerate(json):
         _set, num = pokemon['id'].split('-')
         pokemon_data = (
             i,
@@ -61,10 +73,8 @@ def create_database(pokemon_list: list) -> None:
             None,
             None
         )
-        data.append(pokemon_data)
-    cur.executemany("INSERT INTO POKEMONS VALUES (?, ?, ?, ?, ?, ?, ?, ?)", data)
-    con.commit()
-
+        procesed_cards.append(pokemon_data)
+    return procesed_cards
 
 if __name__ == '__main__':
     main()
